6dd6b493d6e4d56200183207ecf15efdc17dfba5
Trofimchikkk <31827994+Trofimchikkk@users.noreply.github.com>
2019-06-26 02:52:25 +0300

    Update lab#2.py

--- lab#2.py
+++ lab#2.py
@@ -2,24 +2,21 @@ import threading
 import numpy as np
 
 def multi(matrix1, matrix2, n):
-  """
-  функция выполняет произведение двух квадратных матриц произвольной размерности
-  """
-  matrix3 = np.zeros((n,n)) #создаем массив nхn с нулями
+  matrix3 = np.zeros((n,n)) 
   lst = []
   for i in range(n):
     for j in range(n):
-      row = matrix1[i] #берем i-ю строку
-      col = matrix2[j] #берем j-й столбец
-      elem = sum([a*b for a,b in zip(row,col)]) #переумножаем строку на столбец и складываем - получается i,j элемент новой матрицы
+      row = matrix1[i] 
+      col = matrix2[j] 
+      elem = sum([a*b for a,b in zip(row,col)])
       matrix3[i][j]=elem 
   return matrix3
 
 if __name__ == "__main__":
-  X = int(input("Введите размерность квадратных матриц: "))
+  X = int(input("Введите размерность: "))
   A = np.random.randint(10, size=(X,X))
   print(A, "\n")
   B = np.random.randint(10, size=(X,X))
   print(B, "\n")
 
-  threading.Thread(target=lambda a, b, n:print(multi(a,b,n)),args=(A, B, X)).start() #используем lambda-функцию для вывода матрицы на экран
+  threading.Thread(target=lambda a, b, n:print(multi(a,b,n)),args=(A, B, X)).start() 
